7d0a4455df9fce825f9adfe9307a44868f9343ca
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2022-10-22 20:05:45 -0300

    Create customer-orders.jmx file

--- src/main/java/br/com/cams7/orders/App.java
+++ src/main/java/br/com/cams7/orders/App.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+//@EnableAsync
 public class App {
 
   public static void main(String[] args) {
